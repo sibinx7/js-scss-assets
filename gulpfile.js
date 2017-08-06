db3ac9d1492b11ed893887e96a9a929f84a44b7d
Sibin Xavier <sibinx7@gmail.com>
2017-08-06 12:22:16 +0530

    Update structure and packages

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ var gulp = require('gulp');
 var gulpSass = require('gulp-sass');
 var gulpConcat = require('gulp-concat');
 var gulpRename = require('gulp-rename');
+var babel           = require("gulp-babel");
+var gulpBrowserify  = require("gulp-browserify");
+var browserify      = require("browserify");
+var babelify        = require("babelify");
+var source          = require("vinyl-source-stream")
+var buffer          = require('vinyl-buffer');
+var typeScript      = require('gulp-typescript');
+
 
 
 var scssFilePath = './scss/main.scss';
@@ -24,6 +32,15 @@ gulp.task('concatFunctions', function(){
     .pipe(gulp.dest('./js'))
 })
 
+gulp.task('concatBabelScript', function() {
+  return browserify({entries:'./source/js-scss-assets/es6/main.js'})
+    .transform("babelify",{
+      presets:["es2015","latest"]
+    })
+    .bundle()
+    .pipe(source("main-es6.js"))
+    .pipe(gulp.dest('./source/javascripts'))
+});
 
 gulp.task('watch', function(){
     gulp.watch('./source/**/*.js',['concatFunctions']);
